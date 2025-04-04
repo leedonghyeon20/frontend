5a2d277a49224f1b818829e9c08a427e03ceb22b
leedonghyeon <0928dlehdgus@gmail.com>
2025-04-04 17:55:04 +0900

    자바스크립트 기초 (변수, 자료형, 연산자)

---
+++ htmlsource/js/basic/operator.js
@@ -0,0 +1,13 @@
+// 연산자
+// 산술, 비교, 연결
+let a = 4, b = 5;
+
+console.log("a + b = ", a+b)
+console.log("a - b = ", a-b)
+console.log("a * b = ", a*b)
+console.log("a / b = ", a/b)    // 자바 -> 몫(0)
+console.log("a % b = ", a%b)
+
+// + : 연결연산자   숫자 + 문자
+let c = "3";
+console.log("a + c = ", a + c)
