6cf993e37e3af9f98651d652b39d4f97c76653ad
leedonghyeon <0928dlehdgus@gmail.com>
2025-04-10 17:53:17 +0900

    html 자바스크립트 (이벤트 - 화면영역 가져오기, 접근)

---
+++ htmlsource/js/dom/event/product.js
@@ -0,0 +1,50 @@
+const products = [
+  { id: 0, price: 70000, title: "Blossom Dress" },
+  { id: 1, price: 50000, title: "Springfield Shirt" },
+  { id: 2, price: 80000, title: "Black Monastery" },
+];
+
+// 화면 영역 찾아오기
+const cols = document.querySelectorAll(".col-sm-4");
+
+cols.forEach((col) => {
+  console.log(col.querySelector("h5").textContent);
+  console.log(col.querySelector("p").textContent);
+});
+
+// // 배열안의 정보를 이용해 화면에 보여주기
+// products.forEach((item, idx) => {
+//   // 배열요소 가져오기
+//   console.log(idx, item);
+//   // 객체 접근
+//   console.log(item.title);
+
+//   const h5 = document.querySelectorAll("h5");
+//   h5[idx].textContent = item.title;
+//   const p = document.querySelectorAll("p");
+//   p[idx].textContent = item.price;
+// });
+
+// for (let i = 0; i < products.length; i++) {
+//   console.log(products[i]);
+//   console.log(products[i].title);
+// }
+
+cols.forEach((col, idx) => {
+  console.log((col.querySelector("h5").textContent = products[idx].title));
+  console.log((col.querySelector("p").textContent = products[idx].price));
+});
+
+// 다른 방법
+const row = document.querySelector(".row");
+let tags = "";
+products.forEach((item, idx) => {
+  tags += `<div class="col-sm-4>`;
+  tags += `<img src="https:placehold.co/600" alt="" class="w-100" />`;
+  tags += `<h5>${item.title}</h5>`;
+  tags += `<p>가격 : ${item.price}</p>`;
+  tags += `</div>`;
+});
+// console.log(tags);
+// row 자식으로
+row.insertAdjacentHTML("afterbegin", tags);
